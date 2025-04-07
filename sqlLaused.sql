fd53569e9349e03e024f05ad422b5468029d7570
AlanLagunjonok <158563089+AlanLagunjonok@users.noreply.github.com>
2025-04-07 09:33:01 +0300

    Andmite loomine ja Tabeli lisamine

---
+++ sqlLaused.sql
@@ -0,0 +1,26 @@
+-- SQL kommentaar
+-- SQL Server Managment Studio
+-- (localdb)\mssqllocaldb
+-- Authenfication: Windows Auth - admini õigused localhostis
+-- Authenfication: SQL  Server Auth - varem loodud kasutajad
+-- New Query 
+CREATE DATABASE lagunjonokTIT;
+--Object Explorer on vaja pidevalt uuendada käsitsi!
+USE lagunjonokTIT;
+--tabeli loomine
+CREATE TABLE opilane(
+oplilaneID int Primary Key identity(1,1),
+eesnimi varchar(25),
+perenimi varchar(30) Unique,
+synniaeg date,
+aadress TEXT,
+opilaskodu bit
+);
+SELECT * FROM opilane;
+--tebeli kustutamine
+DROP table opilane;
+--andmete lisamine tabelisse
+INSERT INTO opilane(eesnimi, perenimi, synniaeg, aadress, opilaskodu)
+VALUES ('Alan', 'Pavlovich', '2000-12-5', 'Tallinn', 1),
+('Kolya', 'Kuus', '2005-4-1', 'Tallinn', 1),
+('Roma', 'Lenon', '2002-2-7', 'Tallinn', 1);
